0f876c95a45669d3ffac810c8ca736764a193bd4
nowsiwat <nowsiwat@gmail.com>
2024-08-19 15:21:02 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import { Nunito } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import dynamic from 'next/dynamic';
+import { Kanit } from "next/font/google";
 
 const Bootstrap = dynamic(() => import('bootstrap/dist/js/bootstrap.bundle.min'), {
   ssr: false // ไม่โหลดในฝั่ง server
 });
 const nunito = Nunito({ subsets: ["latin"] });
+const kunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
